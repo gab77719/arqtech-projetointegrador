22481f3984e3fc08193ae8b0c062fbda8e03a85b
Gabriel <alves.gabriel3@aluno.ifsp.edu.br>
2025-09-16 16:25:09 -0300

    alterações 16/09

--- projeto_integrador/src/components/Cabecalho/index.jsx
+++ projeto_integrador/src/components/Cabecalho/index.jsx
@@ -5,6 +5,7 @@ export default function Cabecalho() {
     return (
         <header className="cabecalho">
             <img src={Logo} alt="Logo da empresa" className="cabecalho__logo" />
+            <input type="text" placeholder='Pesquisar...'/>
             <nav className="cabecalho__nav">
                 <a href="#home" className="cabecalho__link">Conta</a>
                 <a href="#sobre" className="cabecalho__link">Cadastre-se</a>
---
+++ projeto_integrador/src/components/Nav/index.jsx
@@ -0,0 +1,17 @@
+import "./Nav.css"
+
+export default function Nav() {
+    return (
+        <section className="nav">
+            <nav className="cabecalho__nav">
+                <a href="#home" className="cabecalho__link">Categorias</a>
+                <a href="#sobre" className="cabecalho__link">Ferramentas</a>
+                <a href="#servicos" className="cabecalho__link">Materiais</a>
+                <a href="#servicos" className="cabecalho__link">Parcerias</a>
+            </nav>
+            <img src="" alt="" />
+
+        </section>
+        
+    );
+}
