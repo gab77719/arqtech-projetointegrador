a2a38cc9131534c6e0d375a18f03eaadcb279883
Giovanni <giovannieli2000@gmail.com>
2025-10-04 01:07:00 -0300

    isso ai

--- projeto_integrador/src/components/Footer/Footer.jsx
+++ projeto_integrador/src/components/Footer/Footer.jsx
@@ -13,11 +13,11 @@ function Footer() {
             <img src={Logo} alt='' />
         <div className="footer-content">
             <p>&copy; 2025 Giovanni, Gabriel, Lucas e Kauã. Todos os direitos reservados.</p>
+            </div>
             <div className="social-media">
             <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
             <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
             <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-            </div>
         </div>
         </footer>
 
