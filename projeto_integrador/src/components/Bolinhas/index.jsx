6dc34254819c975bbcfe9afa3cbc681aaebeaf4d
Gabriel <alves.gabriel3@aluno.ifsp.edu.br>
2025-10-13 17:48:39 -0300

    alteração nos icones em azul

--- projeto_integrador/src/components/Bolinhas/index.jsx
+++ projeto_integrador/src/components/Bolinhas/index.jsx
@@ -1,5 +1,9 @@
 import './Bolinha.css'
 import furadeira from '../../assets/furadeira.png';
+import { IoCartOutline } from "react-icons/io5";
+import { SlWallet } from "react-icons/sl";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
 export default function Bolinha() {
   return (
     <>
@@ -9,22 +13,22 @@ export default function Bolinha() {
     <div className="container">
       
       <div className="item">
-        <div className="bolinha">Carro</div>
+        <div className= "bolinha"><IoCartOutline /></div>
         <h3>Compra online</h3>
       </div>
 
       <div className="item">
-        <div className="bolinha">Carro</div>
+        <div className="bolinha"><SlWallet /></div>
         <h3>Parcelamento em até 10x</h3>
       </div>
 
       <div className="item">
-        <div className="bolinha">Carro</div>
+        <div className="bolinha"><CiDeliveryTruck /></div>
         <h3>Entrega rapida</h3>
       </div>
 
       <div className="item">
-        <div className="bolinha">Carro</div>
+        <div className="bolinha"><CiLocationOn /></div>
         <h3>Combine a troca e
         pagamento</h3>
       </div>
